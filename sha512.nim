c44a0a113b52313d1de66a1b6399857a3160da7f
xioren <40043405+xioren@users.noreply.github.com>
2024-01-13 18:41:17 -0800

    Update sha512.nim

--- sha512.nim
+++ sha512.nim
@@ -20,6 +20,7 @@ const initState: array[8, uint64] = [
     0x6a09e667f3bcc908'u64, 0xbb67ae8584caa73b'u64, 0x3c6ef372fe94f82b'u64, 0xa54ff53a5f1d36f1'u64,
     0x510e527fade682d1'u64, 0x9b05688c2b3e6c1f'u64, 0x1f83d9abfb41bd6b'u64, 0x5be0cd19137e2179'u64
 ]
+# NOTE: round constants
 const k: array[80, uint64] = [
     0x428a2f98d728ae22'u64, 0x7137449123ef65cd'u64, 0xb5c0fbcfec4d3b2f'u64, 0xe9b5dba58189dbbc'u64,
     0x3956c25bf348b538'u64, 0x59f111f1b605d019'u64, 0x923f82a4af194f9b'u64, 0xab1c5ed5da6d8118'u64,

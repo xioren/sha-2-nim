05864e1aae8c28c167729964dae873e82de68820
xioren <40043405+xioren@users.noreply.github.com>
2024-01-13 18:47:14 -0800

    Update sha512.nim

--- sha512.nim
+++ sha512.nim
@@ -188,7 +188,7 @@ proc hexDigest*(ctx: Sha512Context): string =
   return result
 
 
-proc newSha512Hash*(msg: openarray[uint8] = @[]): Sha512Context =
+proc newSha512Ctx*(msg: openarray[uint8] = @[]): Sha512Context =
   # NOTE: initialize state
   new result
   result.state = initState
@@ -196,13 +196,13 @@ proc newSha512Hash*(msg: openarray[uint8] = @[]): Sha512Context =
     result.update(msg)
 
 
-proc newSha512Hash*(msg: string): Sha512Context =
-  return newSha512Hash(msg.toOpenArrayByte(0, msg.len.pred))
+proc newSha512Ctx*(msg: string): Sha512Context =
+  return newSha512Ctx(msg.toOpenArrayByte(0, msg.len.pred))
 
 
 when isMainModule:
-  let msg = "some test data"
-  var hash = newSha512Hash(msg)
+  let x = "some test data"
+  var hash = newSha512Ctx(x)
   assert hash.hexDigest() == "4e03a727411d2f658aa530085ad642aec2b032df17f50c8b0b7f044a47017f142db659b14b846ea685d9cd128a78df2137611510e8f8ec139f1bed0a165fbfb8"
   hash.update("some more test data")
   assert hash.hexDigest() == "ed11592e6e6ab2dfdb98d69177da89f6e90743d257a00e7e5cf78af9cdca55def0515a41f5191fc2d6dc90c51eed760812ec70c20b8f25a9838de86bbb9d00b4"

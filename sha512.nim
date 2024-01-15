0c7c708572479c8b862ecdda10349a0be5d1f7a7
xioren <40043405+xioren@users.noreply.github.com>
2024-01-14 20:40:01 -0800

    Update sha512.nim

--- sha512.nim
+++ sha512.nim
@@ -157,8 +157,8 @@ proc digest*(ctx: Sha512Context): array[64, uint8] =
   
   tempCtx.finalize()
   
-  for idx, b in tempCtx.state:
-    bigEndian64(addr result[idx * wordSize], unsafeAddr b)
+  for idx in 0 ..< 8:
+    bigEndian64(addr result[idx * wordSize], unsafeAddr tempCtx.state[idx])
   return result
 
 
@@ -171,8 +171,8 @@ proc hexDigest*(ctx: Sha512Context): string =
   
   tempCtx.finalize()
   result = newStringOfCap(128)
-  for h in tempCtx.state:
-    result.add(h.toHex(16).toLowerAscii())
+  for idx in 0 ..< 8:
+    result.add(tempCtx.state[idx].toHex(16).toLowerAscii())
   return result
 
 
